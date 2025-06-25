508aa9046692f9c3909b121d0fd543d4c9f35625
Baek Un <qordjs700@gmail.com>
2025-06-25 18:00:53 +0900

    init: spring + react 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/example/demo/api/ApiController.java
@@ -0,0 +1,15 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api",method={RequestMethod.GET,RequestMethod.POST})
+public class ApiController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
